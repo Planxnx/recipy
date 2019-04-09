4b51860bb27881fde6b8985aeca64a18bce3c453
Planxnx <60010465@kmitl.ac.th>
2019-04-09 21:40:01 +0700

    fix merge bug

---
+++ src/service/recipe/create_recipe_service.php
@@ -0,0 +1,28 @@
+<?php
+ob_start();
+include("../../../config.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = checkInput($_POST["txtname"]);
+    $description = checkInput($_POST["txtdescription"]);
+    $category = checkInput($_POST["ddlcategory"]);
+    $ingredient = checkInput($_POST["txtingredient"]);
+    $howTo = checkInput($_POST["txthowTo"]);
+    $recipeImg = checkInput($_POST["recipeImg"]);
+
+    $strSQL = "INSERT INTO recipe(name,description,category,ingredient,howTo,logo) VALUES ('" . $name . "','" . $description . "','" . $category . "','" . $ingredient . "','" . $howTo . "','" . $recipeImg . "')";
+    $objQuery = mysqli_query($objCon, $strSQL);
+    header("location:../../../recipe.php?recipeId=" . mysqli_insert_id($objCon));
+    mysqli_close($objCon);
+}
+else{
+    header("location:../../../index.php");
+    mysqli_close($objCon);
+}
+
+function checkInput($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    return $data;
+}
+
+?> 

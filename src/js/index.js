592191654f449a59560ce8be2f333892b67ebfa6
Planxnx <60010465@kmitl.ac.th>
2019-04-21 22:16:18 +0700

    merge index

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,39 @@
-$(document).ready(function () {
-    $('#myModal').css('display', 'block')
+// $(window).load(function () {
+//     if (search === 0) {
+//         $('#myModal').css('display', 'block')
+//         alert(search)
+//     }
+//
+//     $("#closeBtn").click(function () {
+//         $('#myModal').css('display', 'none')
+//     });
+//
+// });
 
-    $("#closeBtn").click(function () {
-        $('#myModal').css('display', 'none')
+$(function () {
+    $("#btnSearch").click(function () {
+        $.ajax({
+            url: "./src/service/search/search.php",
+            type: "post",
+            data: {searchText: $("#searchText").val()},
+            beforeSend: function () {
+                $(".loading").show();
+                $("#list-data").hide();
+            },
+            complete: function () {
+                $(".loading").hide();
+                $("#list-data").show();
+            },
+            success: function (data) {
+                $("#list-data").html(data);
+            }
+        });
     });
-
+    $("#searchform").on("keyup keypress", function (e) {
+        var code = e.keycode || e.which;
+        if (code == 13) {
+            $("#btnSearch").click();
+            return false;
+        }
+    });
 });
--- src/service/search/search.php
+++ src/service/search/search.php
@@ -2,59 +2,48 @@
 //sleep(1);
 include 'searchEngine.php';
 ?>
-    <div class="col-md-12">
-        <table class="table table-bordered">
-            <thead>
-            <th>Search Result</th>
-            <tr>
-                <th></th>
-                <th>name</th>
-                <th>category</th>
 
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $i = 1;
-            foreach ($fullResult as $value) { ?>
-                <tr>
-                    <td><?php echo $i; ?></td>
-                    <td>
-                        <a href="recipe.php?recipeId=<?php echo $value['recipeId'] ?>"><?php echo $value['name']; ?></a>
-                    </td>
-                    <td><?php echo $value['category']; ?></td>
-                </tr>
-                <?php $i++;
+<?php
+$i = 1;
+foreach ($fullResult as $value) { ?>
+    <div class="box-data column">
+        <a href="recipe.php?recipeId=<?php echo $value['recipeId'] ?>">
+            <img style="width: 50%" src="./src/service/recipe/images/<?php echo $value['recipeImg']; ?>">
+            <span class="data-detail"><?php echo $value['name']; ?></span> <br>
+            <span class="data-detail"><?php echo $value['category']; ?></span>
+        </a>
+    </div>
+    <?php $i++;
 //                if($i>10)break;
-            }
-            ?>
-            </tbody>
-        </table>
-        <table class="table table-bordered">
-            <thead>
-            <th>look like your result</th>
+}
+?>
+    </tbody>
+    </table>
+    <table class="table table-bordered">
+        <thead>
+        <th>look like your result</th>
+        <tr>
+            <th></th>
+            <th>name</th>
+            <th>category</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $i = 1;
+        foreach ($fullSimilarResult as $value) { ?>
             <tr>
-                <th></th>
-                <th>name</th>
-                <th>category</th>
+                <td><?php echo $i; ?></td>
+                <td><a href="recipe.php?recipeId=<?php echo $value['recipeId'] ?>"><?php echo $value['name']; ?></a>
+                </td>
+                <td><?php echo $value['category']; ?></td>
             </tr>
-            </thead>
-            <tbody>
-            <?php
-            $i = 1;
-            foreach ($fullSimilarResult as $value) { ?>
-                <tr>
-                    <td><?php echo $i; ?></td>
-                    <td><a href="recipe.php?recipeId=<?php echo $value['recipeId'] ?>"><?php echo $value['name']; ?></a>
-                    </td>
-                    <td><?php echo $value['category']; ?></td>
-                </tr>
-                <?php $i++;
+            <?php $i++;
 //                if($i>10)break;
-            }
-            ?>
-            </tbody>
-        </table>
+        }
+        ?>
+        </tbody>
+    </table>
     </div>
 
 <?php mysqli_close($objCon); ?>

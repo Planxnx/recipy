d14ba61074b379e8c6f91516c96a12ba82e3aa8e
Planxnx <60010465@kmitl.ac.th>
2019-04-23 14:52:27 +0700

    merge editProfile page

--- editProfile.php
+++ editProfile.php
@@ -1,50 +1,88 @@
 <?php
 session_start();
+include("./config.php");
 if (!isset($_SESSION["uid"])) {
     $URL = "sign_in.php";
     echo "<script type='text/javascript'> alert('Please SignIn') </script>";
     echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
     echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
 }
+
+$strSQL = "SELECT * FROM user WHERE uid = '" . trim($_SESSION["uid"]) . "' ";
+$objQuery = mysqli_query($objCon, $strSQL);
+$userResult = mysqli_fetch_array($objQuery);
+
 ?>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/ico" href="src/img/icon.png"/>
+    <title>Recipy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./src/css/default.css">
+    <link rel="stylesheet" href="./src/css/edit_profile.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>
-        function checkPasswordMatch() {
-            var password = $("#txtnewPassword").val();
-            var confirmPassword = $("#txtconNewPassword").val();
-            var conPassword = $("#txtconNewPassword")[0];
-
-            if (password != confirmPassword) {
-                $("#divCheckPasswordMatch").html("Passwords do not match!");
-                conPassword.setCustomValidity('Passwords do not match!');
-            } else {
-                conPassword.setCustomValidity('');
-                $("#divCheckPasswordMatch").html("Passwords match.");
-            }
-        }
-
-        $(document).ready(function () {
-            $("#txtnewPassword, #txtconNewPassword").keyup(checkPasswordMatch);
-        });
-    </script>
 </head>
 <body>
-<form id="userData">
-    Edit Profile <br>
-    Your Password <br>
-    <input required name="txtPassword" pattern="(?=.*[A-Za-z0-9]).{4,16}" title="Please Enter Password" type="password"
-           id="txtPassword"> <br>
-    New Password <br>
-    <input required name="txtnewPassword" pattern="(?=.*[A-Za-z0-9]).{4,16}" title="Please Enter Password"
-           type="password" id="txtnewPassword"> <br>
-    Comfirm New Password <br>
-    <input required name="txtconNewPassword" pattern="(?=.*[A-Za-z0-9]).{4,16}" title="Please Enter Password"
-           type="password" id="txtconNewPassword"> <br>
-    <input id="btnSubmit" type="submit" name="Submit" value="Reset"> &nbsp;
-</form>
+<div id="main">
+    <div id="edit">
+        <div class="detail-column">
+            <div class="detail-header">
+                <span>Your Profile</span>
+            </div>
+            <div class="detail-body">
+                <span>Name : </span> <span style="float:right;"> <?php echo $userResult['name']; ?> </span> <br>
+                <span>Email :</span> <span style="float:right;"> <?php echo $userResult['email']; ?> </span> <br>
+            </div>
+            <div class="detail-footer">
+                <img src="./src//img/logo.png" alt=""> <br>
+            </div>
+        </div>
+        <div class="form-column">
+            <div class="form-header">
+                <span>Reset Your Password</span>
+            </div>
+            <form name="form1" id="userData">
+                <div class="form-data">
+                    <label for="txtPassword">Your Password</label><br>
+                    <input required name="txtPassword" type="password" id="txtPassword"><br>
+                    <label for="txtnewPassword">New Password</label><br>
+                    <input required name="txtnewPassword" type="password" pattern="(?=.*[A-Za-z0-9]).{4,16}"
+                           id="txtnewPassword" onChange="checkPasswordMatch();"><br>
+                    <label for="txtConPassword">Repeat Password</label><br>
+                    <input required name="txtConPassword" type="password" pattern="(?=.*[A-Za-z0-9]).{4,16}"
+                           id="txtConPassword" onChange="checkPasswordMatch();"><br>
+                    <input type="submit" name="btnConfirm" id="btnConfirm" value="Confirm">
+                    <div class="registrationFormAlert" id="divCheckPasswordMatch">
+                    </div>
+                </div>
+            </form>
+            <div class="back-button">
+                <a href="index.php">Back</a>
+            </div>
+        </div>
+
+    </div>
+</div>
 <script type="text/javascript">
+    function checkPasswordMatch() {
+        var password = $("#txtnewPassword").val();
+        var confirmPassword = $("#txtconNewPassword").val();
+        var conPassword = $("#txtconNewPassword")[0];
+
+        if (password != confirmPassword) {
+            $("#divCheckPasswordMatch").html("Passwords do not match!");
+            conPassword.setCustomValidity('Passwords do not match!');
+        } else {
+            conPassword.setCustomValidity('');
+            $("#divCheckPasswordMatch").html("Passwords match.");
+        }
+    }
+
+    $(document).ready(function () {
+        $("#txtnewPassword, #txtconNewPassword").keyup(checkPasswordMatch);
+    });
     $(document).ready(function () {
         $("#btnSubmit").click(function () {
             var valid = this.form.checkValidity();

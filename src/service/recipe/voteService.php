893292955d82c0b34d431ee5eeabdd530be89b46
Planxnx <60010465@kmitl.ac.th>
2019-04-25 20:45:53 +0700

    add feature ranking

--- src/service/recipe/voteService.php
+++ src/service/recipe/voteService.php
@@ -4,6 +4,12 @@ include("../../../config.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $strSQL = "INSERT INTO recipe_vote(uid,recipeId,voteType) VALUES ('" . $_POST["uid"] . "','" . $_POST["recipeId"] . "','" . $_POST["voteType"] . "')";
     $objQuery = mysqli_query($objCon, $strSQL);
+
+    $sql = "SELECT * FROM recipe_vote WHERE recipeId = '" . $_POST['recipeId'] . "' AND voteType = 'like';";
+    $query = mysqli_query($objCon, $sql);
+    $likeCount = mysqli_num_rows($query);
+    $updateSql = "UPDATE recipe SET vote_score = ".$likeCount." WHERE recipeId = '" . $_POST['recipeId'] . "' ";
+    $query = mysqli_query($objCon, $updateSql);
 } else {
     header("location:../../../index.php");
     mysqli_close($objCon);

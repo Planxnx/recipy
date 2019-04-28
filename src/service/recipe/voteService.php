29828a8bdaefc603c6986c5b7d9c75d4ed7e0328
Planxnx <60010465@kmitl.ac.th>
2019-04-29 04:52:46 +0700

    update code

--- src/service/recipe/voteService.php
+++ src/service/recipe/voteService.php
@@ -2,7 +2,7 @@
 session_start();
 include("../../../config.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $strSQL = "INSERT INTO recipe_vote(uid,recipeId,voteType) VALUES ('" . $_POST["uid"] . "','" . $_POST["recipeId"] . "','" . $_POST["voteType"] . "')";
+    $strSQL = "INSERT INTO recipe_vote(uid,recipeId,voteType) VALUES ('" . $_SESSION['uid'] . "','" . $_POST["recipeId"] . "','" . $_POST["voteType"] . "')";
     $objQuery = mysqli_query($objCon, $strSQL);
 
     $sql = "SELECT * FROM recipe_vote WHERE recipeId = '" . $_POST['recipeId'] . "' AND voteType = 'like';";

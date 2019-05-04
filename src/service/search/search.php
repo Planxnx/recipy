936f949b172ee25a1a1c619d5a8fbd3c6862f972
Planxnx <60010465@kmitl.ac.th>
2019-05-05 04:53:49 +0700

    add check nothin found result messege

--- src/service/search/search.php
+++ src/service/search/search.php
@@ -24,25 +24,31 @@ if ($fullResult) {
         <?php $i++;
     }
 }else if ($fullSimilarResult) {
+    if(!$fullResult){
+        echo "<div class='list-header'><span>Your results not found</span></div>";
+    }
     ?>
     <br>
     <hr style="width: 100%;border: 0;height: 0;border-top: 1px solid rgba(0, 0, 0, 0.1);border-bottom: 1px solid rgba(255, 255, 255, 0.3);">
     <div class="list-header">
-        <span>Look like your result</span>
+        <span>Try this!</span>
     </div>
     <?php
     $i = 1;
-    foreach ($fullSimilarResult as $value) { ?>
-        <div class="box-data column">
-            <a href="recipe.php?recipeId=<?php echo $value['recipeId'] ?>">
-                <div class="crop">
-                    <img src="./src/service/recipe/images/<?php echo $value['recipeImg']; ?>">
-                </div>
-                <span style="font-weight: 500;" class="data-detail"><?php echo $value['name']; ?></span> <br>
-                <span style="font-size: 13px;" class="data-detail"><?php echo $value['category']; ?></span>
-            </a>
-        </div>
-        <?php $i++;
+    foreach ($fullSimilarResult as $index => $value) {
+        if ($index < 9 ) {
+            ?>
+            <div class="box-data column">
+                <a href="recipe.php?recipeId=<?php echo $value['recipeId'] ?>">
+                    <div class="crop">
+                        <img src="./src/service/recipe/images/<?php echo $value['recipeImg']; ?>">
+                    </div>
+                    <span style="font-weight: 500;" class="data-detail"><?php echo $value['name']; ?></span> <br>
+                    <span style="font-size: 13px;" class="data-detail"><?php echo $value['category']; ?></span>
+                </a>
+            </div>
+            <?php $i++;
+        }
     }
 }
 ?>

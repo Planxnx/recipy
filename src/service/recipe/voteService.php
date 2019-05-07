449f2a52b694a8ff1a92e15203f6bf76bcb028c5
Planxnx <60010465@kmitl.ac.th>
2019-05-07 16:21:12 +0700

    update unlike

--- src/service/recipe/voteService.php
+++ src/service/recipe/voteService.php
@@ -10,6 +10,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($checkResult)) {
         $strSQL = "INSERT INTO recipe_vote(uid,recipeId,voteType) VALUES ('" . $_SESSION['uid'] . "','" . $_POST["recipeId"] . "','" . $_POST["voteType"] . "')";
         $objQuery = mysqli_query($objCon, $strSQL);
+    }else if($checkResult['voteType'] == $_POST["voteType"]){
+        $strSQL = "DELETE FROM recipe_vote WHERE id = ". $checkResult['id'];
+        $objQuery = mysqli_query($objCon, $strSQL);
     } else if ($checkResult['voteType'] != $_POST["voteType"]) {
         $strSQL = "UPDATE recipe_vote SET voteType = '" . $_POST['voteType'] . "' WHERE recipeId = '" . $_POST['recipeId'] . "'AND uid = '" . $_SESSION['uid'] . "';";
         $objQuery = mysqli_query($objCon, $strSQL);
